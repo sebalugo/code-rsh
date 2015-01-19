0565e1f8d5692ac8e2369348932acc385d15832c
Sebastian Lugo <sebalugo@ubuntu.(none)>
2015-01-19 17:29:17 -0400

    assets precompile

---
+++ public/assets/js_lobby-73e9a786b9f8db7d3da32461bf71f936.js
@@ -0,0 +1,12 @@
+$(function(){
+	var faye = new Faye.Client('http://ancient-lowlands-4557.herokuapp.com/faye');
+	
+	var subs = faye.subscribe("/lobby/js" , function(data){
+		var message = data["message"].content;
+		var newUrl = "/multiplayer?"+"key="+data["message"].key+"&category="+data["message"].category+"&course="+data["message"].course;
+		if(message.indexOf("ready") > -1) window.location.href=newUrl;
+		
+	});
+	
+});
+
