65dadb05a5a06afd445b81589f605c1be6563d7d
Sebastian Lugo <sebalugo@ubuntu.(none)>
2015-01-21 19:33:03 -0400

    test

--- app/assets/javascripts/java_lobby.js
+++ app/assets/javascripts/java_lobby.js
@@ -1,5 +1,5 @@
 $(function(){
-	var faye = new Faye.Client('http://localhost:3000/faye');
+	var faye = new Faye.Client('http://ancient-lowlands-4557.herokuapp.com/faye');
 	
 	var subs = faye.subscribe("/lobby/java" , function(data){
 		var message = data["message"].content;
